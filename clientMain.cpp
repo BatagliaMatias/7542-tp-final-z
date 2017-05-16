da498d0464e66ce788980388d196814fed63f296
Matias Bataglia <matubat91@gmail.com>
2017-05-16 15:01:11 -0300

    compila SDL con nave

--- clientMain.cpp
+++ clientMain.cpp
@@ -1,16 +1,79 @@
 #include <iostream>
 #include "commonSocket.h"
 #include <gtkmm.h>
+#include <SDL2/SDL.h>
 
 
-void on_button_clicked_init_game(){
+void on_jugar_clicked(Glib::RefPtr<Gtk::Application> app){
     std::cout << "Inicia el juego" << std::endl;
+    // variables
+
+    bool quit = false;
+    SDL_Event event;
+    int x = 288;
+    int y = 208;
+
+    // init SDL
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window * window = SDL_CreateWindow("Z",
+                                           SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, 0);
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, 0);
+
+    SDL_Surface * image = SDL_LoadBMP("spaceship.bmp");
+    SDL_Texture * texture = SDL_CreateTextureFromSurface(renderer,
+                                                         image);
+    SDL_FreeSurface(image);
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    // handle events
+
+    while (!quit)
+    {
+        SDL_PollEvent(&event);
+
+        switch (event.type)
+        {
+            case SDL_QUIT:
+                quit = true;
+                break;
+            case SDL_KEYDOWN:
+                switch (event.key.keysym.sym)
+                {
+                    case SDLK_LEFT:  x--; break;
+                    case SDLK_RIGHT: x++; break;
+                    case SDLK_UP:    y--; break;
+                    case SDLK_DOWN:  y++; break;
+                }
+                break;
+            case SDL_BUTTON_LEFT:
+                int mouseX = event.motion.x;
+                int mouseY = event.motion.y;
+                x = mouseX;
+                y = mouseY;
+                break;
+
+        }
+
+        SDL_Rect dstrect = { x, y, 64, 64 };
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, &dstrect);
+        SDL_RenderPresent(renderer);
+    }
+
+    // cleanup SDL
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
 
-void on_button_clicked_exit(){
+void on_salir_clicked(Glib::RefPtr<Gtk::Application> app){
     std::cout << "Chau!" << std::endl;
-    //Salimos del loop de eventos
-    Gtk::Main::quit();
+    app->quit();
 }
 
 int main(int argc, char *argv[]) {
@@ -19,27 +82,22 @@ int main(int argc, char *argv[]) {
    // socket.connectS(argv[1],16332);
    // socket.sendS(argv[2],5);
    // socket.shutdownS();
-    Gtk::Main kit(argc, argv);
-
-    Gtk::Button boton("Iniciar Juego");
-    boton.signal_clicked().connect(sigc::ptr_fun(&on_button_clicked_init_game));
-
-    Gtk::Button botonExit("Salir");
-    botonExit.signal_clicked().connect(sigc::ptr_fun(&on_button_clicked_exit));
-
+    auto app = Gtk::Application::create(argc, argv);
 
     Gtk::Window ventana;
-    Gtk::Box box;
     ventana.set_default_size(800, 640);
-    ventana.set_border_width(10);
-    box.add(boton);
-    box.add(botonExit);
+    Gtk::Box box;
+
+    Gtk::Button jugar("Jugar");
+    Gtk::Button salir("Salir");
+
+    jugar.signal_clicked().connect(sigc::bind(sigc::ptr_fun(on_jugar_clicked), app));
+    salir.signal_clicked().connect(sigc::bind(sigc::ptr_fun(on_salir_clicked), app));
+
+    box.add(jugar);
+    box.add(salir);
     ventana.add(box);
-
-
     ventana.show_all();
 
-
-    Gtk::Main::run(ventana);
-    return 0;
+    return app->run(ventana);
 }
